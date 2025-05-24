30f0aa0e379fb20499c6ffc1084aefedea56f0cd
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-05-24 00:20:16 -0300

    inicio de portfolio publico. Peticiones realizadas con exito. Restan tipos para renderizar objetos

---
+++ portfolio-metamorfose/src/helpers/getCategories.ts
@@ -0,0 +1,17 @@
+const PORT = process.env.NEXT_PUBLIC_API_URL;
+
+export const getCategories = async () => {
+    try {
+        const response = await fetch(`${PORT}/categories`, {
+            method: "GET",
+        });
+        if (!response.ok) {
+            throw new Error("Error en la peticion");
+        }
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
