11220584973fc2fab3724e6ace9586c8af5907a1
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-05-31 15:08:31 -0300

    feat:home finalizada con vista mobile

--- portfolio-metamorfose/src/app/page.tsx
+++ portfolio-metamorfose/src/app/page.tsx
@@ -83,9 +83,9 @@ export default function Home() {
 
 export default function Home() {
   return (
-    <div className="relative">
-      <div className=" flex justify-between items-center">
-        <div className="relative m-3 w-9 h-9 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10 xl:h-10 2xl:w-10 2xl:h-10  ">
+    <div  className=" relative z-0 overflow-x-hidden h-[100vh] w-screen">
+      <div className=" flex justify-between items-center mt-[-10]">
+        <div className="relative m-3 w-9 h-9 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10  xl:h-10 2xl:w-10 2xl:h-10">
           <Image
             src={"/logo.png"}
             alt={"metamorfose-logo"}
@@ -95,42 +95,76 @@ export default function Home() {
         </div>
         <Navbar />
       </div>
-      <div className="absolute w-[25%] h-[119%]  right-6">
+      <div className="fade-right transition-all duration-1000 ease-in-out absolute w-[70%] h-[80%] top-[15%] right-[15%] md:w-[40%] md:h-[110%] md:right-[-15%] md:top-[-10%] z-[-1] ">
         <Image
           src={"/camara-fondo.jpg"}
           alt={"metamorfose-fondo"}
           fill
-          className="object-contain border mask-fade animate-pulse z-[-1]"
+          className="object-contain mask-fade"
         />
       </div>
+      <div className="animate-reveal flex justify-center items-center sm:justify-start ">
       <h1
-        className="font-[family-name:var(--font-bellota)] font-bold
-      mx-[10%] mt-[50%] text-4xl
-      sm:text-7xl sm:mt-[30%] 
+        style={{ textShadow: "3px 3px 10px #676767" }}
+        className=" 
+        font-[family-name:var(--font-bellota)] font-extrabold
+      mt-[30%] text-[2.8rem] 
+      sm:text-6xl sm:text-left sm:ml-15 sm:mt-[30%] 
       md:text-7xl md:mt-[20%] 
       lg:text-8xl lg:mt-[15%]
-      xl:text-9xl  xl:mt-[15%]
-      2xl:text-9xl 2xl:mt-[16%]
-      z-10
-      "
-      >
+      xl:text-[9.5rem]  xl:mt-[15%]
+      2xl:text-[9.5rem] 2xl:mt-[12%]
+      z-10 masked-reveal
+      ">
         METAMORFOSE PH
       </h1>
-
-      <div className="mt-2 ml-[20%] flex justify-start items-center gap-[20rem]">
-      <p className="text-2xl font-[family-name:var(--font-shadows)] tracking-widest}">
-        Viviendo metamorfosis.
-      </p>
-      <ButtonContact />
+      
       </div>
+      <div className="flex justify-center items-center sm:justify-start sm:ml-16 ">
+        <p className="text-2xl font-[family-name:var(--font-shadows)] tracking-widest masked-reveal">
+          Viviendo metamorfosis.
+        </p>
+      </div>
+        <div className="flex justify-center items-center">
+        <ButtonContact />
+        </div>
+        
     </div>
   );
 }
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [labelButton, setLabelButton] = useState<string>("");
+
+  useEffect(() => {
+    isOpen ? setLabelButton(`x`) : setLabelButton(`≡`);
+  }, [isOpen]);
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
   return (
-    <nav className="font-[family-name:var(--font-shadows)] tracking-widest mr-4 flex justify-end">
-      <ul className="flex gap-4">
+    <nav className="relative z-10 font-[family-name:var(--font-shadows)] tracking-widest mt-[-8] mr-2 flex justify-end masked-reveal">
+      <button
+        onClick={openMenu}
+        className={`hover:cursor-pointer top-6 text-white hover:text-gray-400 transition-all duration-300 ease-in-out text-xl mr-6 w-6 sm:hidden animate-pulse`}
+      >
+        {labelButton}
+      </button>
+
+      <ul
+        className={`z-20 absolute gap-3 transform transition-all duration-300 ease-in-out
+  flex flex-col justify-center items-centerm 
+  sm:static sm:flex-row sm:opacity-100 sm:pointer-events-auto 
+  ${
+    isOpen
+      ? "translate-y-10 justify-center items-center opacity-100 pointer-events-auto"
+      : "translate-y-0 justify-center items-center opacity-0 pointer-events-none"
+  }
+  sm:translate-y-0`}
+      >
         <Link className="text-white hover:text-gray-400 duration-300" href="/">
           inicio
         </Link>
@@ -148,11 +182,10 @@ export const Navbar = () => {
   );
 };
 
-
-export const ButtonContact = ()=>{
-  return(
-    <button className="font-[family-name:var(--font-bellota)] tracking-widest  p-2 border border-gray-200 rounded hover:bg-gray-300 hover:text-black duration-300 font-bold hover:scale-110 hover:font hover:cursor-pointer">
-      Quiero una sesión 
+export const ButtonContact = () => {
+  return (
+    <button className="absolute text-center font-[family-name:var(--font-bellota)] tracking-widest w-40  h-10 sm:w-50 sm:h-15 lg:w-45 lg:h-15 xl:w-50 xl:h-15 2xl:w-50  2xl:h-15  mt-[70%]  p-2 border border-gray-200 rounded bg-white text-black hover:text-white duration-300 font-extrabold hover:scale-102 hover:font hover:cursor-pointer z-50 bg-swipe-hover sm:mt-[10%] mask-reveal">
+      Quiero una sesión
     </button>
-  )
-}
+  );
+};
