1f4cf2226516c16ec1a00769e9fcb19a178cc7de
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-05-31 23:46:00 -0300

    feat: nav funcionando correctamente. Se soluciono la superposicion que ocultava el menu desplegable

--- portfolio-metamorfose/src/app/page.tsx
+++ portfolio-metamorfose/src/app/page.tsx
@@ -55,17 +55,24 @@ export default function Home() {
   }, []);
   return (
     <>
-
+      <Navbar />
       <HomeComponent />
-      <p id="portfolio" className="text-2xl">Aqui se mostraran las fotos</p>
-      <div className="grid grid-cols-1">
-        <FotoComponent fotos={fotos} />
-      </div>
 
-      <h1  className="text-4xl">Categorias</h1>
-      <p className="text-2xl">Aqui se mostraran las categorias</p>
-      <div className="">
-        <CategoryComponent categories={categorias} />
+      <div id="portfolio" className="grid grid-cols-1 h-[100vh] ">
+        <p className="text-2xl">Aqui se mostraran las fotos</p>
+        <FotoComponent fotos={fotos} />
+        <h1 className="text-4xl">Categorias</h1>
+        <p className="text-2xl">Aqui se mostraran las categorias</p>
+        <div className="">
+          <CategoryComponent categories={categorias} />
+        </div>
+        <div id="conoceme" className="h-[100vh]">
+<h1>CONOCEME SECTION</h1>
+        </div>
+        <div id="contacto" className="h-[100vh]">
+<h1>CONTACTAME SECTION</h1>
+        </div>
+
       </div>
     </>
   );
@@ -73,19 +80,18 @@ export default function Home() {
 
 export function HomeComponent() {
   return (
-    <div id="home" className="relative z-0 overflow-x-hidden h-[100vh] ">
-            <Navbar />
-                  <div className="relative m-3 w-9 h-9 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10  xl:h-10 2xl:w-10 2xl:h-10">
-        <Image
-          src={"/logo.png"}
-          alt={"metamorfose-logo"}
-          fill
-          className="object-contain "
-        />
+    <div id="home" className="relative z-0 overflow-x-hidden h-[100vh]">
+      <div className=" flex justify-between items-center mt-[-10]">
+        <div className="relative m-3 w-9 h-9 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10  xl:h-10 2xl:w-10 2xl:h-10">
+          <Image
+            src={"/logo.png"}
+            alt={"metamorfose-logo"}
+            fill
+            className="object-contain "
+          />
+        </div>
       </div>
-
-      <div className=" flex justify-between items-center mt-[-10]"></div>
-      <div className="fade-right transition-all duration-1000 ease-in-out absolute w-[70%] h-[80%] top-[15%] right-[15%] md:w-[40%] md:h-[110%] md:right-[-15%] md:top-[-10%] z-[-1]">
+      <div className="fade-right transition-all duration-1000 ease-in-out absolute w-[70%] h-[80%] top-[15%] right-[15%] md:w-[40%] md:h-[110%] md:right-[-15%] md:top-[-10%] z-[-1] ">
         <Image
           src={"/camara-fondo.jpg"}
           alt={"metamorfose-fondo"}
@@ -93,7 +99,7 @@ export function HomeComponent() {
           className="object-contain mask-fade"
         />
       </div>
-      <div className="animate-reveal flex justify-center items-center sm:justify-start">
+      <div className="animate-reveal flex justify-center items-center sm:justify-start ">
         <h1
           style={{ textShadow: "1px 1px 5px #676767,-1px -1px 5px #676767" }}
           className=" 
@@ -103,7 +109,7 @@ export function HomeComponent() {
       md:text-7xl md:mt-[20%] 
       lg:text-8xl lg:mt-[15%]
       xl:text-[9.5rem]  xl:mt-[15%]
-      2xl:text-[9.5rem] 2xl:mt-[16%]
+      2xl:text-[9.5rem] 2xl:mt-[12%]
       z-10 masked-reveal
       "
         >
@@ -131,11 +137,12 @@ export const Navbar = () => {
   }, [isOpen]);
   const openMenu = () => {
     setIsOpen(!isOpen);
+    
     console.log(isOpen);
   };
 
   return (
-    <nav className="fixed z-100 font-[family-name:var(--font-shadows)] tracking-widest flex flex-col justify-center items-end text-[0.9rem] right-2 top-4 masked-reveal">
+    <nav className="fixed z-100 font-[family-name:var(--font-shadows)] tracking-widest flex flex-col justify-center items-end text-[0.9rem] right-2 top-4">
       <button
         onClick={openMenu}
         className={`hover:cursor-pointer text-white hover:text-gray-400 transition-all duration-300 ease-in-out text-xl  w-6 sm:hidden animate-pulse`}
@@ -153,19 +160,32 @@ export const Navbar = () => {
   }
   sm:translate-y-0`}
       >
-        <a className="text-white hover:text-gray-400 duration-300" href="#home">
+        <a
+        onClick={openMenu} 
+        className="text-white hover:text-gray-400 duration-300" href="#home">
           inicio
         </a>
         <a
+          onClick={openMenu}
           className="text-white hover:text-gray-400 duration-300"
           href="#portfolio"
         >
           portfolio
         </a>
-        <a className="text-white hover:text-gray-400 duration-300"
-        href="#conoceme">conoceme</a>
-        <a className="text-white hover:text-gray-400 duration-300"
-        href="#contacto">contacto</a>
+        <a
+          onClick={openMenu}
+          className="text-white hover:text-gray-400 duration-300"
+          href="#conoceme"
+        >
+          conoceme
+        </a>
+        <a
+          onClick={openMenu}
+          className="text-white hover:text-gray-400 duration-300"
+          href="#contacto"
+        >
+          contacto
+        </a>
       </ul>
     </nav>
   );
