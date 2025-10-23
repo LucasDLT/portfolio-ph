18c1b537ad8c1f787261971b8183a2fcc1a37c05
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-10-22 21:59:57 -0300

    feat: modificamos boton de accion principal

--- portfolio-metamorfose/src/components/ButtonContact/index.tsx
+++ portfolio-metamorfose/src/components/ButtonContact/index.tsx
@@ -1,6 +1,6 @@
 export const ButtonContact = () => {
   return (
-    <a href="#contacto" className="text-center absolute font-[family-name:var(--font-bellota)] tracking-widest w-45 h-15 sm:w-50 sm:h-12 lg:w-45 lg:h-12 xl:w-50 xl:h-12 2xl:w-50  2xl:h-12  mt-[70%]  p-2  rounded bg-white text-black hover:text-white transition-all ease-in-out duration-600 font-extrabold hover:scale-102 hover:font-bold hover:cursor-pointer z-50 bg-swipe-hover sm:mt-[6%] masked-reveal">
+    <a href="#contacto" className="text-center absolute font-[family-name:var(--font-bellota)] tracking-widest sm:w-50 lg:w-45 xl:w-50 2xl:w-50  mt-[70%]  lg:p-[0.5%] p-2  rounded-xl bg-white text-black hover:text-white transition-all ease-in-out duration-600 font-extrabold hover:scale-102 hover:font-bold hover:cursor-pointer z-50 bg-swipe-hover sm:mt-[6%] masked-reveal text-[1rem] ">
       Quiero una sesión
     </a>
   );
